70256efb08c7ba7535709cdac4a9276dc5ebfc45
Bowen Cheng <blueicycky@gmail.com>
2018-09-01 14:24:29 -0400

    Fix an error in ProductDetailsComponent

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -18,7 +18,7 @@ export class ProductDetailComponent implements OnInit {
     // $$ The + sign is a JS symbol that converts strings to numbers.
     // The snapshot property provides a static set of initial route parameters which does not change as route parameters change.
     // The parameter name must exactly match the placeholder variable. Name is case sensitive.
-    const productId = +this.getProduct(this.route.snapshot.params['id']);
+    const productId = +this.route.snapshot.params['id'];
     this.getProduct(productId);
   }
 
