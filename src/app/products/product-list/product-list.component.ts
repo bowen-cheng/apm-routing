1fa414b7bfd2ceb1443584e6b7bc23f6907b43c4
Bowen Cheng <blueicycky@gmail.com>
2018-09-02 16:34:41 -0400

    Define and read queryParams across routes

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
@@ -17,15 +18,25 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
 
   ngOnInit(): void {
-    this.productService.getProducts()
-      .subscribe(products => this.products = products,
-        error => this.errorMessage = <any>error);
+    this.productService.getProducts().subscribe(
+      products => this.products = products,
+      error => this.errorMessage = <any>error
+    );
+
+    // $$: Read the query parameters if exists
+    if (!!this.route.snapshot.queryParamMap.get('filterBy')) {
+      this.listFilter = this.route.snapshot.queryParamMap.get('filterBy') || '';
+    }
+    if (!!this.route.snapshot.queryParams['showImage']) {
+      this.showImage = this.route.snapshot.queryParams['showImage'] === 'true';
+    }
   }
 }
