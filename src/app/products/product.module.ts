ff01966936a3824e7a221ffdb1c90bd28ad9d66f
Bowen Cheng <blueicycky@gmail.com>
2018-09-04 11:02:30 -0400

    Register route resolver (ProductResolverService) in product route config

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -8,6 +8,7 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductFilterPipe } from './product-filter.pipe';
 
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductResolverService } from './product-resover.service';
 import { ProductService } from './product.service';
 
 @NgModule({
@@ -16,9 +17,17 @@ import { ProductService } from './product.service';
     RouterModule.forChild([
       // $$ The 'path' property does not need a leading slash but routerLink directive does: <a routerLink='/products'></a>
       { path: 'products', component: ProductListComponent },
+      {
       // $$ The colon (:) symbol identifies a place holder variable, there can be as many as needed as long as they have unique names
-      { path: 'products/:id', component: ProductDetailComponent },
-      { path: 'products/:id/edit', component: ProductEditComponent }
+        path: 'products/:id', component: ProductDetailComponent,
+        // $$ The list of resolvers is key-value pairs, where the key is a logical name, the value is a reference to the resolver service
+        // $$ The key is used for retrieving data later in the component class
+        resolve: { product: ProductResolverService }
+      },
+      {
+        path: 'products/:id/edit', component: ProductEditComponent,
+        resolve: { product: ProductResolverService }
+      }
     ])
   ],
   declarations: [
