ed94d0b1ff8ef62859ebbf299b285152245c3af8
Bowen Cheng <blueicycky@gmail.com>
2018-09-06 12:37:51 -0400

    Retrieve data for child route components using parent's route resolver

--- src/app/products/product-edit-info/product-edit-info.component.ts
+++ src/app/products/product-edit-info/product-edit-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Product } from '../product';
 
 @Component({
   templateUrl: './product-edit-info.component.html'
@@ -8,11 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductEditInfoComponent implements OnInit {
   @ViewChild(NgForm) productForm: NgForm;
 
-  errorMessage: string;
-  product = { id: 1, productName: 'test', productCode: 'test' };
+  protected errorMessage: string;
+  protected product: Product;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // $$ Data for product is retrieved from parent's route resolver
+    this.route.parent.data.subscribe(data => {
+      this.product = data['product'];
+    });
   }
 }
--- src/app/products/product-edit-tags/product-edit-tags.component.ts
+++ src/app/products/product-edit-tags/product-edit-tags.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Product } from '../product';
 
 @Component({
   templateUrl: './product-edit-tags.component.html'
 })
 export class ProductEditTagsComponent implements OnInit {
-  errorMessage: string;
-  newTags = '';
-  product = { id: 1, category: 'test', tags: ['test'] };
+  protected errorMessage: string;
+  protected newTags = '';
+  protected product: Product;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // $$ Data for product is retrieved from parent's route resolver
+    this.route.parent.data.subscribe(data => {
+      this.product = data['product'];
+    });
   }
 
   // Add the defined tags
