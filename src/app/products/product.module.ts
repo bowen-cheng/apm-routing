a0d329881f2bdfae1a3935deb7fd5f1170f56643
Bowen Cheng <blueicycky@gmail.com>
2018-09-08 14:00:45 -0400

    Group product list, detail and edit routes into one component-less route

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -17,27 +17,40 @@ import { ProductService } from './product.service';
   imports: [
     SharedModule,
     RouterModule.forChild([
-      // $$ The 'path' property does not need a leading slash but routerLink directive does: <a routerLink='/products'></a>
-      // Route paths with a leading slash are absolute path. E.g. starting from the root URL
-      { path: 'products', component: ProductListComponent },
       {
-        // $$ The colon (:) symbol identifies a place holder variable, there can be as many as needed as long as they have unique names
-        path: 'products/:id', component: ProductDetailComponent,
-        // $$ The list of resolvers is key-value pairs, where the key is a logical name, the value is a reference to the resolver service
-        // $$ The key, "product", is used for retrieving data later in the component class
-        resolve: { product: ProductResolverService }
-      },
-      {
-        path: 'products/:id/edit', component: ProductEditComponent,
-        resolve: { product: ProductResolverService },
-        // $$ The children array is used for defining child routes
-        // All paths of the child arrays are appended after the path of parent routes
+        // $$ The 'path' property does not need a leading slash but routerLink directive does: <a routerLink='/products'></a>
+        // Route paths with a leading slash are absolute path. E.g. starting from the root URL,
+        path: 'products',
+        // $$ Component-less route: At the parent route level, there is no component matching path: 'products' (the next line is commented)
+        // component: ProductListComponent,
         children: [
-          // $$ full path: products/:id/edit/info
-          { path: 'info', component: ProductEditInfoComponent },
-          // $$ full path: products/:id/edit/tags
-          { path: 'tags', component: ProductEditTagsComponent },
-          { path: '', redirectTo: 'info', pathMatch: 'full' }
+          {
+            // $$ Component-less route: at child route level, the ProductListComponent is displayed as '/products'
+            path: '',
+            component: ProductListComponent
+          },
+          {
+            // $$ The colon (:) symbol identifies a place holder variable, there can be as many as needed as long as they have unique names
+            path: ':id',
+            component: ProductDetailComponent,
+            // $$ The list of resolvers is key-value pairs, where the key is a logical name, the value is a reference to the resolver
+            // The key, "product", is used for retrieving data later in the component class
+            resolve: { product: ProductResolverService }
+          },
+          {
+            path: ':id/edit',
+            component: ProductEditComponent,
+            resolve: { product: ProductResolverService },
+            // $$ The children array is used for defining child routes
+            // All paths of the child arrays are appended after the path of parent routes
+            children: [
+              // $$ full path: products/:id/edit/info
+              { path: 'info', component: ProductEditInfoComponent },
+              // $$ full path: products/:id/edit/tags
+              { path: 'tags', component: ProductEditTagsComponent },
+              { path: '', redirectTo: 'info', pathMatch: 'full' }
+            ]
+          }
         ]
       }
     ])
